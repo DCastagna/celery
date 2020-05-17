2f1e64b082bd2500031f3e97b7ecb4ba9d1dbc7d
Daniele Castagna <daniele.castagna@gmail.com>
2020-05-17 16:30:29 +0000

    Lexical scope and closures

--- src/cel0.c
+++ src/cel0.c
@@ -90,6 +90,21 @@ static cel0_Value* appendValueToVectorInPlace(cel0_Value* vector, cel0_Value* va
   return vector;
 }
 
+static cel0_Value* concatVectorsInPlace(cel0_Value* dest_vector, cel0_Value* append) {
+  assert(dest_vector);
+  assert(append);
+  assert(dest_vector->type == cel0_ValueType_Vector);
+  assert(append->type == cel0_ValueType_Vector);
+  cel0_VectorMetadata* dest_vector_metadata = lookupVectorMetadata(dest_vector->u.vector_id);
+  cel0_VectorMetadata* append_metadata = lookupVectorMetadata(append->u.vector_id);
+  int new_size = dest_vector_metadata->size + append_metadata->size;
+  dest_vector_metadata->vector = realloc(dest_vector_metadata->vector, new_size * sizeof(cel0_Value));
+  assert(new_size == 0 || dest_vector_metadata->vector);
+  memcpy(dest_vector_metadata->vector + dest_vector_metadata->size, append_metadata->vector, append_metadata->size * sizeof(cel0_Value));
+  dest_vector_metadata->size = new_size;
+  return dest_vector;
+}
+
 static cel0_Value* createPanicValue(char* symbol) {
   cel0_Value* value = malloc(sizeof(cel0_Value));
   assert(value);
@@ -194,7 +209,14 @@ void cel0_printValue(cel0_Value* value, FILE* fd) {
 
 static cel0_SymbolBinding* lookupSymbolBinding(cel0_Value* key, cel0_SymbolBindingStack* stack) {
   assert(key->type == cel0_ValueType_Symbol);
-  for (int i=stack->size-1; i>=0; i-=1) {
+  for (int i=stack->size-1; i>=stack->begin; i-=1) {
+    cel0_SymbolBinding* binding = stack->frames + i;
+    assert(binding->symbol->type == cel0_ValueType_Symbol);
+    if (binding->symbol->u.symbol_id == key->u.symbol_id) {
+      return binding;
+    }
+  }
+  for (int i=0; i<stack->global_size; i++) {
     cel0_SymbolBinding* binding = stack->frames + i;
     assert(binding->symbol->type == cel0_ValueType_Symbol);
     if (binding->symbol->u.symbol_id == key->u.symbol_id) {
@@ -204,31 +226,61 @@ static cel0_SymbolBinding* lookupSymbolBinding(cel0_Value* key, cel0_SymbolBindi
   return 0;
 }
 
+static cel0_Value* placeHolderForRecursion() {
+  static cel0_Value* place_holder_for_recursion = 0;
+  if (!place_holder_for_recursion) {
+    place_holder_for_recursion  = createSymbolValue("#self-rec");
+  }
+  return place_holder_for_recursion;
+}
+
 static cel0_Value* eval(cel0_Value* value, cel0_SymbolBindingStack* stack);
 static cel0_Value* apply(cel0_Value* expression, cel0_Value* parameters, cel0_SymbolBindingStack* stack) {
-  assert(expression->type == cel0_ValueType_Vector);
-  cel0_VectorMetadata* expression_metadata = lookupVectorMetadata(expression->u.vector_id);  
-  if (expression_metadata->size != 2 || expression_metadata->vector[0].type != cel0_ValueType_Vector)
+  assert(expression && expression->type == cel0_ValueType_Vector);
+  cel0_VectorMetadata* expression_metadata = lookupVectorMetadata(expression->u.vector_id);
+  if (expression_metadata->size != 2 ||
+      expression_metadata->vector[0].type != cel0_ValueType_Vector)
     return createPanicValue("ill-formed");
   cel0_VectorMetadata* lambda_list_metadata =
     lookupVectorMetadata(expression_metadata->vector->u.vector_id);  
   assert(parameters->type == cel0_ValueType_Vector);
   cel0_VectorMetadata* parameters_metadata = lookupVectorMetadata(parameters->u.vector_id);
-  if (lambda_list_metadata->size != parameters_metadata->size)
+  int number_param = 0;
+  for (; lambda_list_metadata->vector[number_param].type == cel0_ValueType_Symbol  &&
+	 number_param<lambda_list_metadata->size; number_param++);
+
+  if (number_param != parameters_metadata->size)
     return createPanicValue("number-params");
-    
-  int caller_stack_size = stack->size;  
-  for (int i=0; i<parameters_metadata->size; i++) {
-    assert(stack->size < stack->capacity);    
+
+  int caller_stack_begin = stack->begin;
+  stack->begin = stack->size;
+  for (int i=0; i<lambda_list_metadata->size; i++) {
+    assert(stack->size < stack->capacity);
     cel0_SymbolBinding* binding = stack->frames + stack->size;
     binding->type = cel0_SymbolBindingType_Expression;
-    binding->symbol = lambda_list_metadata->vector + i;
-    binding->u.expression = parameters_metadata->vector + i;
+    if (i<number_param) {
+      binding->symbol = lambda_list_metadata->vector + i;
+      binding->u.expression = parameters_metadata->vector + i;
+    } else {
+      assert(lambda_list_metadata->vector[i].type == cel0_ValueType_Vector);
+      cel0_VectorMetadata* param_metadata =
+	lookupVectorMetadata(lambda_list_metadata->vector[i].u.vector_id);
+      assert(param_metadata->size == 2);
+      assert(param_metadata->vector->type == cel0_ValueType_Symbol);
+      binding->symbol = param_metadata->vector;
+      binding->u.expression = param_metadata->vector + 1;
+    }
     stack->size++;
   }
-
+  assert(stack->size < stack->capacity);
+  cel0_SymbolBinding* binding = stack->frames + stack->size;
+  binding->type = cel0_SymbolBindingType_Expression;
+  binding->symbol = placeHolderForRecursion();
+  binding->u.expression = expression;
+  stack->size++;
   cel0_Value* result = eval(expression_metadata->vector + 1, stack);
-  stack->size = caller_stack_size;
+  stack->size = stack->begin;
+  stack->begin = caller_stack_begin;
   return result;
 }
 
@@ -247,13 +299,12 @@ static cel0_Value* eval(cel0_Value* value, cel0_SymbolBindingStack* stack) {
   assert(stack);
 
   if (value->type == cel0_ValueType_Vector) {
-    int caller_stack_size = stack->size;    
     cel0_VectorMetadata* value_metadata = lookupVectorMetadata(value->u.vector_id);
     if (value_metadata->size == 0) return createPanicValue("empty-vec");
 
     assert(value_metadata->size > 0);
     if (value_metadata->vector->type != cel0_ValueType_Symbol) return createPanicValue("not-symbol");
-								 
+    
     cel0_SymbolBinding* binding = lookupSymbolBinding(value_metadata->vector, stack);
     if(!binding) return createPanicValueWithParam("unbound", value_metadata->vector);
     
@@ -271,18 +322,24 @@ static cel0_Value* eval(cel0_Value* value, cel0_SymbolBindingStack* stack) {
     }
     if (binding->type == cel0_SymbolBindingType_TransformNative ||
 	binding->type == cel0_SymbolBindingType_Native) {
-      cel0_Value* ret =  binding->u.native(parameters, stack);
+      cel0_Value* ret =  binding->u.native.expression(parameters, stack);
       if (ret->type == cel0_ValueType_Panic)
 	return appendValueToVectorInPlace(ret, createEvalPanicStackEntry(binding->symbol, parameters));
       return ret;
     } else {
       assert(binding->type == cel0_SymbolBindingType_Expression);
-      cel0_Value* ret = apply(binding->u.expression, parameters, stack);
+      cel0_Value* expression = binding->u.expression;
+      if (expression->type == cel0_ValueType_Symbol && expression->u.symbol_id == placeHolderForRecursion()->u.symbol_id) {
+	cel0_SymbolBinding* self_rec_binding = lookupSymbolBinding(expression, stack);
+	if (!self_rec_binding) return createPanicValue("self-rec-binding");
+	assert(self_rec_binding->type == cel0_SymbolBindingType_Expression);
+	expression = self_rec_binding->u.expression;
+      }
+      cel0_Value* ret = apply(expression, parameters, stack);
       if (ret->type == cel0_ValueType_Panic)
 	return appendValueToVectorInPlace(ret, createEvalPanicStackEntry(binding->symbol, parameters));
       return ret;
     }
-    stack->size = caller_stack_size;
   } else if (value->type == cel0_ValueType_Number) {
     return value;
   } else if (value->type == cel0_ValueType_Symbol) {
@@ -305,7 +362,7 @@ static cel0_Value* bind(cel0_Value* params, cel0_SymbolBindingStack* stack) {
   assert(params->type == cel0_ValueType_Vector);
   cel0_VectorMetadata* params_metadata = lookupVectorMetadata(params->u.vector_id);
   if (params_metadata->size < 2) return createPanicValueWithParam("ill-formed", params);
-  int caller_stack_size = stack->size;  
+  int caller_stack_size = stack->size;
   for (int i=0; i<params_metadata->size-1; i+=1) {
     if (params_metadata->vector[i].type != cel0_ValueType_Vector) return createPanicValueWithParam("ill-formed", params);
     cel0_VectorMetadata* binding_metadata =
@@ -315,18 +372,142 @@ static cel0_Value* bind(cel0_Value* params, cel0_SymbolBindingStack* stack) {
       return createPanicValue("ill-formed");
 
     assert(stack->size < stack->capacity);
-    cel0_Value* bound_expression = eval(binding_metadata->vector + 1, stack);
-    if (bound_expression->type == cel0_ValueType_Panic) return bound_expression;
-  
-    cel0_SymbolBinding* binding = stack->frames + stack->size;
+    cel0_SymbolBinding* binding = stack->frames + stack->size;    
     binding->type = cel0_SymbolBindingType_Expression;
     binding->symbol = binding_metadata->vector;
-    binding->u.expression = bound_expression;
+    binding->u.expression = placeHolderForRecursion();
     stack->size++;
+    cel0_Value* bound_expression = eval(binding_metadata->vector + 1, stack);
+    if (bound_expression->type == cel0_ValueType_Panic) return bound_expression;    
+    binding->u.expression = bound_expression;    
   }
+
   cel0_Value* result = eval(params_metadata->vector + params_metadata->size - 1, stack);
   stack->size = caller_stack_size;
   return result;
+}
+
+static cel0_Value* captureLexicalBindings(cel0_Value* expression, cel0_SymbolBindingStack* stack) {
+  assert(expression);
+  if (expression->type == cel0_ValueType_Symbol) {
+    cel0_SymbolBinding* binding = lookupSymbolBinding(expression, stack);
+    if (!binding) return createPanicValueWithParam("unbound", expression);
+    if (binding->type != cel0_SymbolBindingType_Expression) return createVectorValue();
+    if (binding->symbol == binding->u.expression) return createVectorValue();
+    cel0_Value* entry = appendValueToVectorInPlace(createVectorValue(), binding->symbol);
+    entry = appendValueToVectorInPlace(entry, binding->u.expression);
+    return appendValueToVectorInPlace(createVectorValue(), entry);
+  } else if (expression->type == cel0_ValueType_Vector) {
+    cel0_VectorMetadata* expression_metadata = lookupVectorMetadata(expression->u.vector_id);
+    if (expression_metadata->size == 0) return createPanicValue("empty-vec");
+    if (expression_metadata->vector->type != cel0_ValueType_Symbol) createPanicValue("not-symbol");
+    cel0_SymbolBinding* binding = lookupSymbolBinding(expression_metadata->vector, stack);
+    if (!binding) return createPanicValueWithParam("unbound", expression_metadata->vector);
+    if (binding->type == cel0_SymbolBindingType_TransformNative) {
+      assert(binding->u.native.captureLexicalBindings);
+      cel0_Value* params = createVectorValue();
+      for (int i=1; i<expression_metadata->size; i++)
+	params = appendValueToVectorInPlace(params, expression_metadata->vector + i);
+      return binding->u.native.captureLexicalBindings(params, stack);
+    } else {
+      cel0_Value* result = createVectorValue();      
+      for (int i=0; i<expression_metadata->size; i++) {
+	cel0_Value* param_result = captureLexicalBindings(expression_metadata->vector + i, stack);
+	if (param_result->type == cel0_ValueType_Panic) return param_result;	
+	result = concatVectorsInPlace(result, param_result);	  
+      }
+      return result;
+    }
+  } else if (expression->type == cel0_ValueType_Number) {
+    return createVectorValue();
+  }
+  assert(0);
+}
+
+static cel0_Value* bindCaptureLexicalBindings(cel0_Value* params, cel0_SymbolBindingStack* stack) {
+  assert(params);
+  assert(params->type == cel0_ValueType_Vector);
+  cel0_VectorMetadata* params_metadata = lookupVectorMetadata(params->u.vector_id);
+  if (params_metadata->size < 2) return createPanicValueWithParam("ill-formed", params);
+
+  cel0_Value* result = createVectorValue();
+  int caller_stack_size = stack->size;
+  for (int i=0; i<params_metadata->size-1; i++) {
+    if (params_metadata->vector[i].type != cel0_ValueType_Vector) return createPanicValueWithParam("ill-formed", params);
+    cel0_VectorMetadata* binding_metadata =
+      lookupVectorMetadata(params_metadata->vector[i].u.vector_id);    
+
+    if (binding_metadata->size != 2 || binding_metadata->vector->type != cel0_ValueType_Symbol)
+      return createPanicValue("ill-formed");
+
+    assert(stack->size < stack->capacity);
+    cel0_SymbolBinding* binding = stack->frames + stack->size;
+    binding->type = cel0_SymbolBindingType_Expression;
+    binding->symbol = binding_metadata->vector;
+    binding->u.expression = placeHolderForRecursion();
+    stack->size++;    
+    cel0_Value* bindings = captureLexicalBindings(binding_metadata->vector + 1, stack);
+    if (bindings->type == cel0_ValueType_Panic) return bindings;
+    binding->u.expression = binding_metadata->vector;
+    result = concatVectorsInPlace(result, bindings);
+
+  }
+  cel0_Value* body_bindings = captureLexicalBindings(params_metadata->vector + params_metadata->size - 1,stack);
+  if (body_bindings->type == cel0_ValueType_Panic) return body_bindings;  
+  result = concatVectorsInPlace(result, body_bindings);
+  stack->size = caller_stack_size;
+  return result;
+}
+
+static cel0_Value* ifCaptureLexicalBindings(cel0_Value* params, cel0_SymbolBindingStack* stack) {
+  assert(params);
+  assert(params->type == cel0_ValueType_Vector);
+  cel0_VectorMetadata* params_metadata = lookupVectorMetadata(params->u.vector_id);    
+  if(params_metadata->size != 3) return createPanicValue("ill-formed");
+
+  cel0_Value* result = createVectorValue();
+  for (int i=0; i<3; i++) {
+    cel0_Value* bindings = captureLexicalBindings(params_metadata->vector + i,stack);    
+    if (bindings->type == cel0_ValueType_Panic) return bindings;  
+    result = concatVectorsInPlace(result, bindings);
+  }
+  return result;
+}
+
+static cel0_Value* quoteCaptureLexicalBindings(cel0_Value* params, cel0_SymbolBindingStack* stack) {
+  (void)params;
+  (void)stack;
+  return createVectorValue();
+}
+
+static cel0_Value* lambdaCaptureLexicalBindings(cel0_Value* params, cel0_SymbolBindingStack* stack) {
+  assert(params);
+  assert(params->type == cel0_ValueType_Vector);  
+  assert(stack);
+  cel0_VectorMetadata* params_metadata = lookupVectorMetadata(params->u.vector_id);  
+  if (params_metadata->size != 2 || 
+      params_metadata->vector[0].type != cel0_ValueType_Vector) {
+    return createPanicValue("ill-formed");
+  }
+  cel0_VectorMetadata* lambda_list_metadata = lookupVectorMetadata(params_metadata->vector->u.vector_id);    
+  for (int i=0; i<lambda_list_metadata->size; i++) {
+    if (lambda_list_metadata->vector[i].type != cel0_ValueType_Symbol)
+      return createPanicValueWithParam("lambda-list-ill-formed", lambda_list_metadata->vector + i);
+  }
+  int caller_stack_size = stack->size;
+  for (int i=0; i<lambda_list_metadata->size; i++) {
+    assert(stack->size < stack->capacity);    
+    cel0_SymbolBinding* binding = stack->frames + stack->size;
+    binding->type = cel0_SymbolBindingType_Expression;
+    binding->symbol = lambda_list_metadata->vector + i;
+    binding->u.expression = lambda_list_metadata->vector + i;
+    stack->size++;
+  }
+
+  cel0_Value* body_bindings = captureLexicalBindings(params_metadata->vector + 1, stack);
+  stack->size = caller_stack_size;
+  
+  return body_bindings;
 }
 
 static cel0_Value* lambda(cel0_Value* params, cel0_SymbolBindingStack* stack) {
@@ -339,7 +520,20 @@ static cel0_Value* lambda(cel0_Value* params, cel0_SymbolBindingStack* stack) {
     return createPanicValue("ill-formed");
   }
 
-  return params;
+  cel0_VectorMetadata* lambda_list_metadata = lookupVectorMetadata(metadata->vector->u.vector_id);    
+  for (int i=0; i<lambda_list_metadata->size; i++) {
+    if (lambda_list_metadata->vector[i].type != cel0_ValueType_Symbol)
+      return createPanicValueWithParam("lambda-list-ill-formed", lambda_list_metadata->vector + i);
+  }
+  cel0_Value* params_with_capture = createVectorValue();
+  params_with_capture = concatVectorsInPlace(params_with_capture, metadata->vector);
+  cel0_Value* captures = lambdaCaptureLexicalBindings(params, stack);
+  if (captures->type == cel0_ValueType_Panic) return captures;
+  params_with_capture = concatVectorsInPlace(params_with_capture, captures);
+
+  cel0_Value* result = createVectorValue();
+  result = appendValueToVectorInPlace(result, params_with_capture);
+  return appendValueToVectorInPlace(result, metadata->vector + 1);
 }
 
 static cel0_Value* quote(cel0_Value* params, cel0_SymbolBindingStack* stack) {
@@ -526,33 +720,33 @@ cel0_Value* cel0_eval(cel0_Value* value) {
   int native = cel0_SymbolBindingType_Native;  
 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = transform, .symbol = createSymbolValue("bind"), .u = {.native = bind}};
+    { .type = transform, .symbol = createSymbolValue("bind"), .u = {.native  = { bind, bindCaptureLexicalBindings}}};
   frames[size++] = (cel0_SymbolBinding)
-    { .type = transform, .symbol = createSymbolValue("lambda"), .u = {.native = lambda}};
+    { .type = transform, .symbol = createSymbolValue("lambda"), .u = {.native = { lambda, lambdaCaptureLexicalBindings }}};
   frames[size++] = (cel0_SymbolBinding)
-    { .type = transform, .symbol = createSymbolValue("if"), .u = {.native = ifStatement}}; 
+    { .type = transform, .symbol = createSymbolValue("if"), .u = {.native = { ifStatement, ifCaptureLexicalBindings } }}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = transform, .symbol = createSymbolValue("quote"), .u = {.native = quote}};
+    { .type = transform, .symbol = createSymbolValue("quote"), .u = {.native =  { quote, quoteCaptureLexicalBindings }}};
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("add"), .u = {.native = add}}; 
+    { .type = native, .symbol = createSymbolValue("add"), .u = {.native = {add}}}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("mul"), .u = {.native = mul}}; 
+    { .type = native, .symbol = createSymbolValue("mul"), .u = {.native = {mul}}}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("eq"), .u = {.native = eq}};
+    { .type = native, .symbol = createSymbolValue("eq"), .u = {.native = {eq}}};
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("length"), .u = {.native = length}}; 
+    { .type = native, .symbol = createSymbolValue("length"), .u = {.native = {length}}}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("append"), .u = {.native = append}}; 
+    { .type = native, .symbol = createSymbolValue("append"), .u = {.native = {append}}}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("nth"), .u = {.native = nth}}; 
+    { .type = native, .symbol = createSymbolValue("nth"), .u = {.native = {nth}}}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("open-file!"), .u = {.native = open_file}}; 
+    { .type = native, .symbol = createSymbolValue("open-file!"), .u = {.native = {open_file}}}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("dp!"), .u = {.native = debug_print}}; 
+    { .type = native, .symbol = createSymbolValue("dp!"), .u = {.native = {debug_print}}}; 
   frames[size++] = (cel0_SymbolBinding)
-    { .type = native, .symbol = createSymbolValue("vec"), .u = {.native = vector}}; 
+    { .type = native, .symbol = createSymbolValue("vec"), .u = {.native = {vector}}}; 
   
-  cel0_SymbolBindingStack stack = {.frames = frames, .size = size, .capacity = capacity };
+  cel0_SymbolBindingStack stack = {.frames = frames, .global_size = size, .begin = size, .size = size, .capacity = capacity };
   
   stack.frames = frames;
   return eval(value, &stack);
--- src/cel0.h
+++ src/cel0.h
@@ -26,12 +26,18 @@ typedef struct cel0_SymbolBinding {
   cel0_Value* symbol;
   union {
     cel0_Value* expression;
-    cel0_Value* (*native)(cel0_Value* parameters, struct cel0_SymbolBindingStack* stack);
+    struct {
+      cel0_Value* (*expression)(cel0_Value* parameters, struct cel0_SymbolBindingStack* stack);
+      cel0_Value* (*captureLexicalBindings)(cel0_Value* parameters, struct cel0_SymbolBindingStack* stack);
+    } native;
   } u;
 } cel0_SymbolBinding;
 
 typedef struct cel0_SymbolBindingStack {
+  struct cel0_SymbolBindingStack* previous;
   cel0_SymbolBinding* frames;
+  int global_size;
+  int begin;
   int size;
   int capacity;
 } cel0_SymbolBindingStack;
@@ -41,4 +47,3 @@ cel0_Value* cel0_parse(char* code);
 void cel0_printValue(cel0_Value* value, FILE* fd);
 
 cel0_Value* cel0_eval(cel0_Value* value);
-
